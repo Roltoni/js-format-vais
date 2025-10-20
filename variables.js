aae1ddeaca054a44aa861774d58b868d5cdb5c64
User <nikolajevs04@gmail.com>
2025-10-20 08:55:03 +0300

    3 uzddevums

---
+++ variables.js
@@ -0,0 +1,11 @@
+const string = "Pt25";
+number("23");
+Boolean
+if (Boolean(true)) {
+  console.log("Vērtība IR patiesa");
+}
+if (Boolean(false)) {
+  console.log("vērtība NAV patiesa");
+}
+
+EULER =2.718
